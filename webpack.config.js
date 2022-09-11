a3a14a7060d4d9e93441b6b7743109663304c4bb
Ramesh Vishnoi <rameshvishnoi90904@gmail.com>
2022-09-11 11:35:44 +0530

    fix: webpack deployment changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,56 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require("webpack").container.ModuleFederationPlugin;
 const deps = require('./package.json').dependencies
+const path = require('path');
 
 module.exports = {
-    mode: 'development',
     devServer: {
         port: 8082,
     },
+    // output: {
+    //   filename: 'main.js',
+    // },
     output: {
-        filename: 'main.js',
-      },
-      module: {
-        rules: [{ 
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            options: { presets: [
-                '@babel/env',
-                ["@babel/preset-react", {"runtime": "automatic"}]
-            ] },
-         }, { 
-            test: /\.css$/, 
-            use: [ 'style-loader', 'css-loader' ] 
-        }],
-      },
+      path: path.resolve(__dirname, 'build'), // change this
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{ 
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          options: { presets: [
+              '@babel/env',
+              ["@babel/preset-react", {"runtime": "automatic"}]
+          ] },
+        }, { 
+          test: /\.css$/, 
+          use: [ 'style-loader', 'css-loader' ] 
+      }],
+    },
     plugins: [
-        new ModuleFederationPlugin({
-            name: "bookingModule",
-            filename: "remoteEntry.js",
-            remotes: {},
-            exposes: {
-              "./Counter":"./src/Counter.js"
+      new ModuleFederationPlugin({
+          name: "bookingModule",
+          filename: "remoteEntry.js",
+          remotes: {},
+          exposes: {
+            "./Counter":"./src/Counter.js"
+          },
+          shared: {
+            ...deps,
+            react: {
+              singleton: true,
+              requiredVersion: deps.react,
             },
-            shared: {
-              ...deps,
-              react: {
-                singleton: true,
-                requiredVersion: deps.react,
-              },
-              "react-dom": {
-                singleton: true,
-                requiredVersion: deps["react-dom"],
-              },
+            "react-dom": {
+              singleton: true,
+              requiredVersion: deps["react-dom"],
             },
-          }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
+          },
+        }),
+      new HtmlWebpackPlugin({
+          template: './public/index.html'
+      })
     ]
 };
